be945d199e20b6aa6fe657a0e26f0a797cc18b70
Elliott Linder <elliott.darfink@gmail.com>
2018-04-15 12:35:53 +0200

    feat(dm): add character slot range

--- murust-data-model/src/types/character/mod.rs
+++ murust-data-model/src/types/character/mod.rs
@@ -1,7 +1,11 @@
 pub use self::class::Class;
 pub use self::guild::GuildRole;
 pub use self::hero::HeroStatus;
+use std::ops::Range;
 
 mod class;
 mod guild;
 mod hero;
+
+/// The range of slots availabe for a character.
+pub const CHARACTER_SLOTS: Range<usize> = 0..5;
